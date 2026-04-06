b6f3e5f89199728f6d053dfe708d0fd449bba841
maicol <michael.borja@gmail.com>
2026-04-05 22:39:28 -0500

    Agregando Netlify TOML y actualizando Vite para React Router

--- itq-frontend/vite.config.js
+++ itq-frontend/vite.config.js
@@ -3,10 +3,11 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  server: {
-    port: 5173,
-    proxy: {
-      '/api': 'http://localhost:8080'
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: undefined
+      }
     }
   }
 })
